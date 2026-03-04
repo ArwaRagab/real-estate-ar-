4408590ca4bffc052e347be4a18b4a351c3f6119
arwa <arwaragab61@gmail.com>
2026-03-04 15:16:55 +0200

    improvement

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ let menuToggle = document.getElementById("menu-toggle");
 let navLinks = document.querySelector(".nav-links");
 
 menuToggle.onclick = function () {
-//  e.stopPropagation(); 
     navLinks.classList.toggle("active");
 };
+
+let backgroundOption = true;
+let backgroundInterval;
+
+let landingPage = document.querySelector('.landing-page');
+let imgsArray =["one.jpg","two.jpg","three.jpg","five.jpg","six.jpg","four (1).jpg","landing.jpg"];
+
+function randomizeImgs(){
+    if(backgroundOption === true){
+        backgroundInterval = setInterval(() => {
+            let randomNumber = Math.floor(Math.random() * imgsArray.length);
+            landingPage.style.backgroundImage = 'url("./images/' + imgsArray[randomNumber] + '")';
+        }, 3000);
+    }
+}
+randomizeImgs();
